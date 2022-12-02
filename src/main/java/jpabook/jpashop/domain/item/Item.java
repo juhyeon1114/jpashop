b0e64071a7ecbac1b270311599419436aada1c5d
Kai Kim <juhyeon1114@gmail.com>
2022-12-02 10:25:12 +0900

    complete : Item 도메인 레포, 서비스 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,15 +2,14 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -26,4 +25,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    /**
+     * 재고 증가
+     */
+    public void addStock(int q) {
+        this.stockQuantity += q;
+    }
+
+    /**
+     * 재고 감소
+     */
+    public void removeStock(int q) throws NotEnoughStockException {
+        int resetStock = this.stockQuantity = q;
+        if (resetStock < 0) {
+            throw new NotEnoughStockException("Need more stock");
+        }
+        this.stockQuantity -= q;
+    }
 }
